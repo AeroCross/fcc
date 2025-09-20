fe94d9a763ce68442d1edeae71d4c67b70a7db0c
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-09-21 09:23:27 +1000

    Move all common code to the Base class

--- calculator/annual.rb
+++ calculator/annual.rb
@@ -1,36 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "base"
+
 module Calculator
-  class Annual
-    MINIMUM_PRINCIPAL_AMOUNT = 1000 # dollars
+  class Annual < Base
     MINIMUM_TERM_LENGTH = 12 # months
 
-    # @param principal [Integer] Principal deposit, in dollars
-    # @param rate [Float] Interest rate per annum
-    # @param term [Integer] How long the investment term is, in months
-    def initialize(principal:, rate:, term:)
-      @principal = principal.to_f
-      @rate = rate.to_f
-      @term = term.to_f
-    end
-
-    def call
-      ensure_valid_principal
-      ensure_valid_term
-      calculate
-    end
-
     private
-
-    attr_reader :principal, :rate, :term
-
-    def ensure_valid_principal
-      raise Error::InvalidPrincipal if principal < MINIMUM_PRINCIPAL_AMOUNT
-    end
-
-    def ensure_valid_term
-      raise Error::InvalidTerm if term < MINIMUM_TERM_LENGTH
-    end
 
     def calculate
       (principal * (1 + annual_interest_rate)**term_in_years).round
