bc985ae88dca40d1dc42b2e3203781d636a5a10c
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-09-21 08:33:59 +1000

    Do the same with the Quarterly calculator
    At this stage, it's only the calculation formula, the name of methods
    (for clarity, since I'm not inputting the entire formula as is into the
    #calculate method) and the spec data that is different, so I'm able to
    copy the previous calculator and make adjustments appropriately.

---
+++ calculator/quarterly.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Calculator
+  class Quarterly
+    MINIMUM_PRINCIPAL_AMOUNT = 1000 # dollars
+    MINIMUM_TERM_LENGTH = 3 # months
+
+    # @param principal [Integer] Principal deposit, in dollars
+    # @param rate [Float] Interest rate per annum
+    # @param term [Integer] How long the investment term is, in months
+    def initialize(principal:, rate:, term:)
+      @principal = principal.to_f
+      @rate = rate.to_f
+      @term = term.to_f
+    end
+
+    def call
+      ensure_valid_principal
+      ensure_valid_term
+      calculate
+    end
+
+    private
+
+    def ensure_valid_principal
+      raise Error::InvalidPrincipal if @principal < MINIMUM_PRINCIPAL_AMOUNT
+    end
+
+    def ensure_valid_term
+      raise Error::InvalidTerm if @term < MINIMUM_TERM_LENGTH
+    end
+
+    def calculate
+      (@principal * (1 + quarterly_interest_rate)**quarters_in_term).round
+    end
+
+    def quarterly_interest_rate
+      (@rate / 100.0) / 4.0
+    end
+
+    def quarters_in_term
+      (@term / 12.0) * 4.0
+    end
+  end
+end
---
+++ specs/calculator/quarterly_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require_relative "../../calculator/quarterly"
+require_relative "../../calculator/error"
+
+module Calculator
+  RSpec.describe Quarterly do
+    subject(:result) do
+      described_class.new(
+        principal: principal,
+        rate: rate,
+        term: term
+      ).call
+    end
+
+    describe "reference scenarios" do
+      context "when compared to the reference example" do
+        let(:principal) { 10_000 }
+        let(:rate) { 1.10 }
+        let(:term) { 36 } # 3 years
+
+        it "returns the reference result" do
+          expect(result).to eq(10_335)
+        end
+      end
+
+      context "when all the values are different from the reference" do
+        let(:principal) { 98_300 }
+        let(:rate) { 4.05 }
+        let(:term) { 60 } # 5 years
+
+        it "returns the correct final balance" do
+          expect(result).to eq(120_242)
+        end
+      end
+    end
+
+    describe "term scenarios" do
+      context "when the term is not a full year" do
+        let(:principal) { 10_000 }
+        let(:rate) { 2.39 }
+        let(:term) { 59 } # 4 years, 11 months
+
+        it "returns the correct final balance" do
+          expect(result).to eq(11_243)
+        end
+      end
+
+      context "when the term length is the same as the minimum" do
+        let(:principal) { 1_000 }
+        let(:rate) { 1.10 }
+        let(:term) { 3 }
+
+        it "returns the correct final balance" do
+          expect(result).to eq(1_003)
+        end
+      end
+    end
+
+    describe "principal deposit scenarios" do
+      context "when the principal is the same as the minimum" do
+        let(:principal) { 1_000 }
+        let(:rate) { 1.10 }
+        let(:term) { 3 }
+
+        it "returns the correct final balance" do
+          expect(result).to eq(1_003)
+        end
+      end
+    end
+
+    describe "interest rate scenarios" do
+      context "when the interest rate is different from the reference" do
+        let(:principal) { 10_000 }
+        let(:rate) { 3.43 }
+        let(:term) { 36 }
+
+        it "returns the correct final balance" do
+          expect(result).to eq(11_079)
+        end
+      end
+
+      context "when the interest rate is an integer" do
+        let(:principal) { 10_000 }
+        let(:rate) { 2 }
+        let(:term) { 36 }
+
+        it "returns the correct final balance" do
+          expect(result).to eq(10_617)
+        end
+      end
+    end
+
+    describe "invalid scenarios" do
+      context "when the principal is lower than the minimum" do
+        let(:principal) { 900 }
+        let(:rate) { 1.10 }
+        let(:term) { 12 }
+
+        it "raises an error" do
+          expect { result }.to raise_error(Error::InvalidPrincipal)
+        end
+      end
+
+      context "when the term length is lower than the minimum" do
+        let(:principal) { 12_000 }
+        let(:rate) { 2.30 }
+        let(:term) { 2 }
+
+        it "raises an error" do
+          expect { result }.to raise_error(Error::InvalidTerm)
+        end
+      end
+    end
+  end
+end
